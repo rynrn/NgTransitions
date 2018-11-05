0c20b4ff513e6f0819352aa83edbf6f0df775c93
Nadav Avisror <rynrn@walla.co.il>
2018-11-05 23:45:37 +0200

    tsc

---
+++ directive/src/ng-transitions/ng-transitions.d.ts
@@ -0,0 +1,20 @@
+import { ElementRef, OnInit, OnDestroy } from '@angular/core';
+export declare class NgTransitionsDirective implements OnInit, OnDestroy {
+    el: ElementRef;
+    NgTransition: any;
+    indexPositionInList: number;
+    private parent;
+    private enterAnimationName;
+    private leaveAnimationName;
+    private enterAnimationDelay;
+    private leaveAnimationDelay;
+    private enterDuration;
+    private leaveDuration;
+    private appendTo;
+    constructor(el: ElementRef);
+    ngOnInit(): void;
+    ngOnDestroy(): void;
+    private reTouchedToDOMOnDestroy;
+    private removeFromDom;
+    private setParent;
+}
